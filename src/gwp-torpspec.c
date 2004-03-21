ef7c3d2225172b13656a67c125784c74cb89f8e2
Lucas Di Pentima <lucas@di-pentima.com.ar>
2004-03-21 22:57:15 +0000

    torpedoes specs type

---
+++ src/gwp-torpspec.c
@@ -0,0 +1,271 @@
+/*
+ *  Gnome War Pad: A VGA Planets Client for Gnome
+ *  Copyright (C) 2002-2004 Lucas Di Pentima <lucas@lunix.com.ar>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "gwp-torpspec.h"
+
+/*
+ * Private members.
+ */
+struct _GwpTorpSpecPrivate {
+  gboolean dispose_has_run;
+  /* Actual data */
+  gint id;
+  GString *name; /* max 20 chars */
+  gint torpedo_cost;
+  gint launcher_cost;
+  gint tritanium; /* mineral cost for launcher, torps take 1kt of */
+  gint duranium;  /* every mineral */
+  gint molybdenum;
+  gint mass; /* launcher mass, torp mas is always 1 */
+  gint tech_level;
+  gint kill_value;
+  gint damage_value;
+};
+
+/*
+ * Forward definitions.
+ */
+static void gwp_torpspec_init (GTypeInstance *instance, gpointer g_class);
+static void gwp_torpspec_class_init (GwpTorpSpecClass *klass);
+
+GType gwp_torpspec_get_type (void)
+{
+  static GType type = 0;
+  if (type == 0) {
+    static const GTypeInfo info = {
+      sizeof (GwpTorpSpecClass),
+      NULL, /* base_init */
+      NULL, /* base_finalize */
+      (GClassInitFunc) gwp_torpspec_class_init, /* class_init */
+      NULL, /* class_finalize */
+      NULL, /* class_data */
+      sizeof (GwpTorpSpec),
+      0, /* n_preallocs */
+      (GInstanceInitFunc) gwp_torpspec_init /* instance_init */
+    };
+    type = g_type_register_static (G_TYPE_OBJECT,
+				   "GwpTorpSpec",
+				   &info, 0);
+  }
+  return type;
+}
+
+/* Instance constructor */
+static void gwp_torpspec_init (GTypeInstance *instance,
+			       gpointer       g_class)
+{
+  GwpTorpSpec *self = (GwpTorpSpec *)instance;
+  self->priv = g_new0 (GwpTorpSpecPrivate, 1);
+  self->priv->dispose_has_run = FALSE;
+
+  /* Private members init */
+  self->priv->id = 0;
+  self->priv->name = g_string_new("");
+  self->priv->torpedo_cost = 0;
+  self->priv->launcher_cost = 0;
+  self->priv->tritanium = 0;
+  self->priv->duranium = 0;
+  self->priv->molybdenum = 0;
+  self->priv->mass = 0;
+  self->priv->tech_level = 0;
+  self->priv->kill_value = 0;
+  self->priv->damage_value = 0;
+}
+
+static void gwp_torpspec_dispose (GwpTorpSpec *self)
+{
+  if (self->priv->dispose_has_run) {
+    return;
+  }
+  /* Make sure dispose does not run twice. */
+  self->priv->dispose_has_run = TRUE;
+
+  /*
+   * Here I have to unref all members on which I own a reference.
+   */
+  g_string_free (self->priv->name, TRUE);
+}
+
+static void gwp_torpspec_finalize (GwpTorpSpec *self)
+{
+  /* Here, complete object destruction. */
+  g_free (self->priv);
+}
+
+static void gwp_torpspec_class_init (GwpTorpSpecClass *klass)
+{
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+  /* Register destructor methods. */
+  gobject_class->dispose = gwp_torpspec_dispose;
+  gobject_class->finalize = gwp_torpspec_finalize;
+}
+
+/*
+ * Public method implementations.
+ */
+GwpTorpSpec * gwp_torpspec_new (void)
+{
+  return g_object_new (gwp_torpspec_get_type (), NULL);
+}
+
+/**************************/
+/* get/set boring methods */
+/**************************/
+
+gint gwp_torpspec_get_id (GwpTorpSpec *self)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+
+  return self->priv->id;
+}
+
+void gwp_torpspec_set_id (GwpTorpSpec *self, gint id)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+  g_assert (id >= 1 && id <= 10);
+
+  self->priv->id = id;
+}
+
+gint gwp_torpspec_get_torpedo_cost (GwpTorpSpec *self)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+
+  return self->priv->torpedo_cost;
+}
+
+void gwp_tospspec_set_torpedo_cost (GwpTorpSpec *self, gint tc)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+  g_assert (tc >= 0);
+
+  self->priv->torpedo_cost = tc;
+}
+
+gint gwp_torpspec_get_launcher_cost (GwpTorpSpec *self)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+
+  return self->priv->launcher_cost;
+}
+
+void gwp_torpspec_set_launcher_cost (GwpTorpSpec *self, gint lc)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+  g_assert (lc >= 0);
+
+  self->priv->launcher_cost = lc;
+}
+
+gint gwp_torpspec_get_tritanium (GwpTorpSpec *self)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+
+  return self->priv->tritanium;
+}
+
+void gwp_torpspec_set_tritanium (GwpTorpSpec *self, gint tri)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+  g_assert (tri >= 0);
+
+  self->priv->tritanium = tri;
+}
+
+gint gwp_torpspec_get_duranium (GwpTorpSpec *self)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+
+  return self->priv->duranium;
+}
+
+void gwp_torpspec_set_duranium (GwpTorpSpec *self, gint dur)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+  g_assert (dur >= 0);
+
+  self->priv->duranium = dur;
+}
+
+gint gwp_torpspec_get_molybdenum (GwpTorpSpec *self)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+
+  return self->priv->molybdenum;
+}
+
+void gwp_torpspec_set_molybdenum (GwpTorpSpec *self, gint mol)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+  g_assert (mol >= 0);
+
+  self->priv->molybdenum = mol;
+}
+
+gint gwp_torpspec_get_mass (GwpTorpSpec *self)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+
+  return self->priv->mass;
+}
+
+void gwp_torpspec_set_mass (GwpTorpSpec *self, gint mass)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+  g_assert (mass >= 0);
+
+  self->priv->mass = mass;
+}
+
+gint gwp_torpspec_get_tech_level (GwpTorpSpec *self)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+
+  return self->priv->tech_level;
+}
+
+void gwp_torpspec_set_tech_level (GwpTorpSpec *self, gint tl)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+  g_assert (tl >= 1 && tl <= 10);
+
+  self->priv->tech_level = tl;
+}
+
+gint gwp_torpspec_get_kill_value (GwpTorpSpec *self)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+
+  return self->priv->kill_value;
+}
+
+void gwp_torpspec_set_kill_value (GwpTorpSpec *self, gint kv)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+  g_assert (kv >= 0);
+
+  self->priv->kill_value = kv;
+}
+
+gint gwp_torpspec_get_damage_value (GwpTorpSpec *self)
+{
+  g_assert (GWP_IS_TORPSPEC(self));
+
+  return self->priv->damage_value;
+}
---
+++ src/gwp-torpspec.h
@@ -0,0 +1,100 @@
+/*
+ *  Gnome War Pad: A VGA Planets Client for Gnome
+ *  Copyright (C) 2002-2004 Lucas Di Pentima <lucas@lunix.com.ar>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * GwpTorpSpec:
+ * -----------
+ *
+ * Starship's Torpedoes Specification Type - it describes all the torpedo
+ * weapons on the game, the data is loaded from the TORPSPEC.DAT file.
+ */
+
+#ifndef GWP_TORPSPEC_H
+#define GWP_TORPSPEC_H
+
+#include <glib.h>
+#include <glib-object.h>
+
+/*
+ * Type macros.
+ */
+
+typedef struct _GwpTorpSpec GwpTorpSpec;
+typedef struct _GwpTorpSpecClass GwpTorpSpecClass;
+typedef struct _GwpTorpSpecPrivate GwpTorpSpecPrivate;
+
+struct _GwpTorpSpec {
+  GObject parent;
+
+  /* instance members */
+
+  /* private */
+  GwpTorpSpecPrivate *priv;
+};
+
+struct _GwpTorpSpecClass {
+  GObjectClass parent;
+  /* class members */
+};
+
+/* used by GWP_TORPSPEC_TYPE */
+GType gwp_torpspec_get_type (void);
+
+/*
+ * Public method definitions.
+ */
+GwpTorpSpec *gwp_torpspec_new (void);
+
+
+
+/**************************/
+/* get/set boring methods */
+/**************************/
+gint gwp_torpspec_get_id (GwpTorpSpec *self);
+void gwp_torpspec_set_id (GwpTorpSpec *self, gint id);
+gint gwp_torpspec_get_torpedo_cost (GwpTorpSpec *self);
+void gwp_tospspec_set_torpedo_cost (GwpTorpSpec *self, gint tc);
+gint gwp_torpspec_get_launcher_cost (GwpTorpSpec *self);
+void gwp_torpspec_set_launcher_cost (GwpTorpSpec *self, gint lc);
+gint gwp_torpspec_get_tritanium (GwpTorpSpec *self);
+void gwp_torpspec_set_tritanium (GwpTorpSpec *self, gint tri);
+gint gwp_torpspec_get_duranium (GwpTorpSpec *self);
+void gwp_torpspec_set_duranium (GwpTorpSpec *self, gint dur);
+gint gwp_torpspec_get_molybdenum (GwpTorpSpec *self);
+void gwp_torpspec_set_molybdenum (GwpTorpSpec *self, gint mol);
+gint gwp_torpspec_get_mass (GwpTorpSpec *self);
+void gwp_torpspec_set_mass (GwpTorpSpec *self, gint mass);
+gint gwp_torpspec_get_tech_level (GwpTorpSpec *self);
+void gwp_torpspec_set_tech_level (GwpTorpSpec *self, gint tl);
+gint gwp_torpspec_get_kill_value (GwpTorpSpec *self);
+void gwp_torpspec_set_kill_value (GwpTorpSpec *self, gint kv);
+gint gwp_torpspec_get_damage_value (GwpTorpSpec *self);
+
+
+/*
+ * Standard defines.
+ */
+#define GWP_TORPSPEC_TYPE (gwp_torpspec_get_type())
+#define GWP_TORPSPEC(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), GWP_TORPSPEC_TYPE, GwpTorpSpec))
+#define GWP_TORPSPEC_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST ((klass), GWP_TORPSPEC_TYPE, GwpTorpSpecClass))
+#define GWP_IS_TORPSPEC(obj) (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GWP_TORPSPEC_TYPE))
+#define GWP_IS_TORPSPEC_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GWP_TORPSPEC_TYPE
+#define GWP_TORPSPEC_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS ((obj), GWP_TORPSPEC_TYPE, GwpTorpSpecClass)) 
+
+#endif
