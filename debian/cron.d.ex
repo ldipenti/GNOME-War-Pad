128ba19027efc7b8a16b608d4106cea23224e8aa
Lucas Di Pentima <lucas@di-pentima.com.ar>
2004-04-15 20:24:10 +0000

    Debian package generation!! woohoooo!!

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the gwp package
+#
+0 4	* * *	root	gwp_maintenance
---
+++ debian/gwp-default.ex
@@ -0,0 +1,10 @@
+# Defaults for gwp initscript
+# sourced by /etc/init.d/gwp
+# installed at /etc/default/gwp by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
