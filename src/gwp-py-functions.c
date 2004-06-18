b0c6ec1312eddb8d7858b327a85c546df4960476
Lucas Di Pentima <lucas@di-pentima.com.ar>
2004-06-18 23:58:58 +0000

    new file: generic gwp python module funcs

---
+++ src/gwp-py-functions.c
@@ -0,0 +1,43 @@
+/*
+ *  Gnome War Pad: A VGA Planets Client for Gnome
+ *  Copyright (C) 2002-2004 Lucas Di Pentima <lucas@lunix.com.ar>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * This file contains the functions that will be mapped to the Python
+ * environment, but don't belong to any defined class.
+ */
+
+#include "gwp-py-functions.h"
+
+
+
+/************************/
+/**** Ship functions ****/
+/************************/
+GwpShip * ship_get_by_id (gint id) 
+{
+  return gwp_ship_get (ship_list, id);
+}
+
+/************************/
+/*** Planet functions ***/
+/************************/
+GwpPlanet * planet_get_by_id (gint id)
+{
+  return gwp_planet_get (planet_list, id);
+}
---
+++ src/gwp-py-functions.h
@@ -0,0 +1,32 @@
+/*
+ *  Gnome War Pad: A VGA Planets Client for Gnome
+ *  Copyright (C) 2002-2004 Lucas Di Pentima <lucas@lunix.com.ar>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef GWP_PY_FUNCTIONS_H
+#define GWP_PY_FUNCTIONS_H
+
+#include <gnome.h>
+#include "gwp-ship.h"
+#include "gwp-planet.h"
+
+GwpShip * ship_get_by_id (gint id);
+
+GwpPlanet * planet_get_by_id (gint id);
+
+
+#endif
