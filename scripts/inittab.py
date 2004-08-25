68c0f378a35cdd94c5eed0878069bf4ae375508c
Lucas Di Pentima <lucas@di-pentima.com.ar>
2004-08-25 23:06:30 +0000

    Register a new PluginManager on load

--- scripts/inittab.py
+++ scripts/inittab.py
@@ -1,13 +1,14 @@
 # Initial script loaded by GWP
-
 class PluginManager:
     __module__ = 'gwp'
     
     def manage_key_event (self, event):
-        print "Key pressed: " + event
+        if (event.type == gtk.gdk.KEY_PRESS):
+            print "Key press: %s\n" % (event.string)
 
 if __name__ == "__main__":
     # Register clases on 'gwp' module
     gwp.__dict__["PluginManager"] = PluginManager    
     # Initialize a PluginManager and pass it to GameState in C
-    pass
+    gwp.register_plugin_mgr (gwp.PluginManager())
+
