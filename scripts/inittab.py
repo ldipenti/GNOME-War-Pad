554c31006a393ac2c5983f73ee1033ae8f7ffe49
Lucas Di Pentima <lucas@di-pentima.com.ar>
2004-08-26 20:04:23 +0000

    Some work being done on the plugin framework

--- scripts/inittab.py
+++ scripts/inittab.py
@@ -10,20 +10,67 @@ import gwp
 class PluginManager:
     __module__ = 'gwp'
 
-    # Key-press hooks dictionary
-    key_hooks = {}
+    # Private attributes
+    __key_hooks = {}
+    __plugins = []
     
     def manage_event_key (self, event):
         if (event["type"] == gtk.gdk.KEY_PRESS):
-            self.key_hooks[event["string"]]()
+            self.__key_hooks[event["string"]]()
 
     def set_hook_key (self, key, action):
-        self.key_hooks[key] = action
+        self.__key_hooks[key] = action
 
+    def register_plugin (self, plugin):
+        try:
+            plugin.register(self)
+        except NotImplementedError:
+            pass
+        else:
+            self.__plugins.append (plugin)
+
+class Plugin:
+    """
+    Plugin class, all plugins will need to use this class to register
+    its functions on the plugin manager.
+    """
+    __module__ = 'gwp'
+
+    # Custom attributes
+    plugin_name__ = ''
+    plugin_version__ = 0
+    plugin_author_name__ = ''
+    plugin_author_email__ = ''
+    plugin_desc_short__ = ''
+    plugin_desc_long__ = ''
+    plugin_license__ = ''
+
+    # Constructor
+    def __init__ (self, name, version, author_name,
+                  author_email, desc_s, desc_l, license):
+        self.plugin_name__ = name
+        self.plugin_version__ = version
+        self.plugin_author_name__ = author_name
+        self.plugin_author_email__ = author_email
+        self.plugin_desc_short__ = desc_s
+        self.plugin_desc_long__ = desc_l
+        self.plugin_license__ = license
+
+    # Executed at registration time
+    def register (self):
+        raise NotImplementedError
+
+    # Executed at elimination time
+    def unregister (self):
+        raise NotImplementedError
+
+##############
 # Main code execute at load
+##############
 if __name__ == "__main__":
     # Register clases on 'gwp' module
-    gwp.__dict__["PluginManager"] = PluginManager    
+    gwp.__dict__["PluginManager"] = PluginManager
+    gwp.__dict__["Plugin"] = Plugin
     # Initialize a PluginManager and pass it to GameState in C
     gwp.set_plugin_mgr (gwp.PluginManager ())
 
