96ed4f4ff6ef7ef1508c3bc389244d51c4538681
Lucas Di Pentima <lucas@di-pentima.com.ar>
2007-01-17 21:52:10 +0000

    gnomevfs dependency eliminated from filereaders

--- src/gwp/filereaders/casefile.py
+++ src/gwp/filereaders/casefile.py
@@ -1,39 +1,39 @@
 # CaseInsensitiveFile class: search for a name with similar filename. This is
 # because VGAP files usually come from DOS environments.
 
-import gnomevfs
+#import gnomevfs
 import sre
 import os.path
 import os
 
-# Buggy code, maybe GnomeVFS's fault??
-class CaseInsensitiveFileVFS(gnomevfs.Handle):
-    '''
-    Search for a file name which can be case swapped
-    '''
-    def __init__(self, filename):
-        uri = gnomevfs.URI(filename)
-        # Get parent dir contents
-        files = []
-        for f in gnomevfs.DirectoryHandle(uri.parent):
-            files.append(f)
-        # Collect similar file names
-        matches = []
-        for f in files:
-            regex = uri.short_name.replace('.', '\.')
-            if sre.search(regex, f.name, sre.IGNORECASE):
-                matches.append(f)
-        # Check for unique match
-        if len(matches) == 1:
-            print "Found: " + matches[0].name
-            real_uri = gnomevfs.URI(str(uri.parent) + '/' + matches[0].name)
-            super(CaseInsensitiveFile, self).__init__(real_uri)
-            self.size = self.get_file_info().size
-        else:
-            print "OUCH! found " + str(len(matches)) + " matches"
-            raise gnomevfs.NotFoundError, "There were zero or more than one matches on your file name"
-        return
-    pass
+## Buggy code, maybe GnomeVFS's fault??
+#class CaseInsensitiveFileVFS(gnomevfs.Handle):
+#    '''
+#     Search for a file name which can be case swapped
+#     '''
+#     def __init__(self, filename):
+#         uri = gnomevfs.URI(filename)
+#         # Get parent dir contents
+#         files = []
+#         for f in gnomevfs.DirectoryHandle(uri.parent):
+#             files.append(f)
+#         # Collect similar file names
+#         matches = []
+#         for f in files:
+#             regex = uri.short_name.replace('.', '\.')
+#             if sre.search(regex, f.name, sre.IGNORECASE):
+#                 matches.append(f)
+#         # Check for unique match
+#         if len(matches) == 1:
+#             print "Found: " + matches[0].name
+#             real_uri = gnomevfs.URI(str(uri.parent) + '/' + matches[0].name)
+#             super(CaseInsensitiveFile, self).__init__(real_uri)
+#             self.size = self.get_file_info().size
+#         else:
+#             print "OUCH! found " + str(len(matches)) + " matches"
+#             raise gnomevfs.NotFoundError, "There were zero or more than one matches on your file name"
+#         return
+#     pass
 
 class CaseInsensitiveFileNOVFS(file):
     def __init__(self, filename):
