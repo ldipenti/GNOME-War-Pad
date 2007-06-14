be24c4c13db8083d90d3ac33cfc9771f9b90b335
Cristian Abalos <abalosc@gmail.com>
2007-06-14 12:30:11 +0000

    Added ignore property to avoid list of *.pyc in the status report Object Data was Moved to right panel in glade Race names are readed from player data

--- src/starchartview.py
+++ src/starchartview.py
@@ -6,46 +6,76 @@ pygtk.require('2.0')
 import gtk, gobject, cairo, pango
 import math
 
-from gwp.collections import PlanetCollection, ShipCollection
+from gwp.collections import PlanetCollection, ShipCollection, RaceList
 from gwp.widgets import Starchart, PlanetDrawable, ShipDrawable
 
 from kiwi.ui.delegates import SlaveDelegate, Delegate
 from kiwi.ui.gadgets import quit_if_last
 from kiwi.ui.objectlist import Column, ObjectList
 
-
 import os.path
+
+class GameData:
+    path = None
+    racenum = None
+    racelist = None
+    state = False
+    
+    def __init__(self):
+        pass
+
+    def is_valid(self):
+        return self.state
+
+    def set_path(self, path):
+        if path != "" and os.path.isdir(path):
+            # TODO : verify path
+            path += '/'
+            self.path = path
+
+            # Set gral data
+            self.state = True
+            self.racelist = RaceList( self.get_path() )
+
+    def get_path(self):
+        return self.path
+
+    def get_racenum(self):
+        # TODO: read the data from path
+        return 7
+
+    def get_racelist(self):
+        return self.racelist
 
 class Shell(Delegate):
     my_widgets = ["entry_dir", "button_open"]
     planets = None
     ships = None
-    planetslave = None
     starchart = None
+    planetslave = None
+    objlist = None
     
     def __init__(self):
         Delegate.__init__(self, gladefile="starchartview",
                           widgets=self.my_widgets,
                           delete_handler=quit_if_last)
-
-        self.planetslave = PlanetData()
-        self.attach_slave("eventbox2", self.planetslave)
-
+     
         # object list
         self.objlist = ObjectList([Column('id', data_type=int, sorted=True),
                                    Column('name', data_type=str),
                                    ])
         slave = SlaveDelegate(toplevel=self.objlist)
         self.attach_slave("eventbox3", slave)
-
+            
 
     def on_button_open__clicked(self, widget):
         path = self.entry_dir.get_text()
         
         if path != "" and os.path.isdir(path):
-            path += '/'
-            self.planets = PlanetCollection(path, 7)
-            self.ships = ShipCollection(path, 7)
+            gd = GameData()
+            gd.set_path(path)
+            self.planets = PlanetCollection( gd.get_path(), gd.get_racenum() )
+            self.ships = ShipCollection( gd.get_path(), gd.get_racenum() )
             self.starchart = Starchart()
             self.starchart.add_drawables(self.planets, PlanetDrawable)
             self.starchart.add_drawables(self.ships, ShipDrawable)
@@ -57,8 +87,13 @@ class Shell(Delegate):
             slave.focus_toplevel()
             self.slave = slave
 
+            # object data
+            self.planetslave = PlanetData(gd)
+            self.attach_slave("eventbox2", self.planetslave)
+
+
     def on_starchart_selected(self, widget, x, y, data=None):
-        min_dist = 99999999
+        min_dist = 99999
         ret = None
         objlist = []
         objects = self.planets.values() + self.ships.values()
@@ -75,8 +110,8 @@ class Shell(Delegate):
         self.starchart.move(ret.x - x, ret.y - y)
 
         # Reset model to none so it wont complain of different models assignments
-        #self.planetslave.proxy.set_model(None)
-        #self.planetslave.proxy.set_model(ret)
+        self.planetslave.proxy.set_model(None)
+        self.planetslave.proxy.set_model(ret)
         
         self.objlist.clear()
         self.objlist.extend(objlist)
@@ -86,26 +121,20 @@ class PlanetData(SlaveDelegate):
     planetwidgets = ["name", "owner", "id"]
     proxy = None
     
-    def __init__(self):
+    def __init__(self, gd):
         SlaveDelegate.__init__(self, gladefile="planetdata",
                                toplevel_name="window1",
                                widgets=self.planetwidgets)
 
         self.proxy = self.add_proxy(model=None, widgets=self.planetwidgets)
-        self.owner.prefill([
-            ("desconocido", -1),
-            ("Feds", 1),
-            ("Lizard", 2),
-            ("Birdmen", 3),
-            ("Fascists", 4),
-            ("Privateers", 5),
-            ("Borg", 6),
-            ("Crystal People", 7),
-            ("Evils", 8),
-            ("Robots", 9),
-            ("Rebels", 10),
-            ("Colonies of Man", 11),
-            ])
+        races = []
+
+        if gd.is_valid():
+            racelist = gd.get_racelist()
+            races = racelist.get_short_and_number()
+            races.append ( ("desconocido", -1) )
+            
+        self.owner.prefill( races )
 
 
 
